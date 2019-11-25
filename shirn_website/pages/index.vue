7ee45bf9f2d7af84b2090579ed19564b6cd6cd87
ibonny <ibonny@gmail.com>
2019-11-25 18:10:52 +0000

    Saving changes.

--- shirn_website/pages/index.vue
+++ shirn_website/pages/index.vue
@@ -1,13 +1,23 @@
  <template>
   <div class="container">
     <div>
-        <span class="shirn">Once upon a time, there was a beautiful woman.</span>
+        <input type="text" v-model="text" /><br/>
+        <br/>
+        <span class="shirn">{{ text }}</span><br/>
+        <span class="dardanian">{{ text }}</span><br/>
+        <span class="nirichaen">{{ text }}</span><br/>
+        <span class="tengwar">{{ text }}</span>
     </div>
   </div>
 </template>
 
 <script>
 export default {
+    "data": function() {
+        return {
+            "text": ""
+        }
+    }
 }
 </script>
 
@@ -22,8 +32,38 @@ body {
     src: url("/shirn.otf")
 }
 
+@font-face {
+    font-family: 'dardanian';
+    src: url("/dardanian.ttf");
+}
+
+@font-face {
+    font-family: 'nirichaen';
+    src: url('/nirichaen.ttf');
+}
+
+@font-face {
+    font-family: "tengwar";
+    src: url("/tngani.ttf");
+}
+
 .shirn {
     font-family: "shirn", "Georgia", serif;
+    font-size: 30px;
+}
+
+.dardanian {
+    font-family: "dardanian", "Georgia", serif;
+    font-size: 30px;
+}
+
+.nirichaen {
+    font-family: "nirichaen", "Georgia", serif;
+    font-size: 30px;
+}
+
+.tengwar {
+    font-family: "tengwar", "Georgia", serif;
     font-size: 30px;
 }
 </style>
