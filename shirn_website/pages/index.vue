8a32d967492327add166532fb5ceb3e926b1046d
ibonny <ibonny@gmail.com>
2019-11-25 16:18:28 +0000

    Initial render.

---
+++ shirn_website/pages/index.vue
@@ -0,0 +1,29 @@
+ <template>
+  <div class="container">
+    <div>
+        <span class="shirn">Once upon a time, there was a beautiful woman.</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+body {
+    margin-top: 20px;
+    margin-left: 20px;
+}
+
+@font-face {
+    font-family: 'shirn';
+    src: url("/shirn.otf")
+}
+
+.shirn {
+    font-family: "shirn", "Georgia", serif;
+    font-size: 30px;
+}
+</style>
