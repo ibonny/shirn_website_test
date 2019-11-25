6b500762f8e529d106abe3a242b2bced661f986c
ibonny <ibonny@gmail.com>
2019-11-25 21:10:12 +0000

    Saving changes with Engangri.

--- shirn_website/pages/index.vue
+++ shirn_website/pages/index.vue
@@ -1,14 +1,15 @@
  <template>
-  <div class="container">
-    <div>
-        <input type="text" v-model="text" /><br/>
-        <br/>
-        <span class="shirn">{{ text }}</span><br/>
-        <span class="dardanian">{{ text }}</span><br/>
-        <span class="nirichaen">{{ text }}</span><br/>
-        <span class="tengwar">{{ text }}</span>
+    <div class="container">
+        <div>
+            <input type="text" v-model="text" /><br/>
+            <br/>
+            <span class="shirn">{{ text }}</span><br/>
+            <span class="dardanian">{{ text }}</span><br/>
+            <span class="nirichaen">{{ text }}</span><br/>
+            <span class="tengwar">{{ text }}</span><br/>
+            <span class="enganagri">{{ text }}</span>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -16,6 +17,11 @@ export default {
     "data": function() {
         return {
             "text": ""
+        }
+    },
+    "head": function() {
+        return {
+            "title": "Various font tests"
         }
     }
 }
@@ -47,6 +53,11 @@ body {
     src: url("/tngani.ttf");
 }
 
+@font-face {
+    font-family: "enganigri";
+    src: url("/Enganagri.ttf")
+}
+
 .shirn {
     font-family: "shirn", "Georgia", serif;
     font-size: 30px;
@@ -64,6 +75,11 @@ body {
 
 .tengwar {
     font-family: "tengwar", "Georgia", serif;
+    font-size: 30px;
+}
+
+.enganagri {
+    font-family: "enganagri", "Georgia", serif;
     font-size: 30px;
 }
 </style>
